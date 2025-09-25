7b267eef593b5b3fbb9111839539ceacccfc3827
Trusty <ivanbordeira2015@hotmail.com>
2025-09-25 19:41:37 -0300

    Implementación recursiva

--- TrianguloDeCaracter.pas
+++ TrianguloDeCaracter.pas
@@ -19,11 +19,53 @@ Ingrese una dimension: 4
 
 *)
 
+procedure generarCaracter(caracter: char; dimension: integer);
+begin
+    if dimension = 1 then
+     write(caracter)
+  else
+    begin
+      write(caracter);
+      write(' ');
+      generarCaracter(caracter, dimension - 1);
+    end;
+end;
+
+procedure generarEspacios(espacios: integer);
+begin
+  if (espacios = 1) or (espacios = 0) then
+     write(' ')
+  else
+    begin
+      generarEspacios(espacios - 1);
+      write(' ')
+    end;
+  end;
+
+procedure crearTriangulo(caracter: char; dimension: integer);
+var
+  espacios: integer;
+begin
+  espacios := dimension;
+
+  generarEspacios(espacios);
+  generarCaracter(caracter, dimension);
+end;
+
+procedure TrianguloDeCaracterRecursivo(caracter: char; dimension: integer);
+  begin
+    if dimension = 1 then
+         crearTriangulo(caracter, 1)
+    else
+    begin
+         TrianguloDeCaracterRecursivo(caracter,dimension - 1);
+         writeln('');
+         crearTriangulo(caracter,dimension)
+    end;
+  end;
 
 
-
-
-program TrianguloDeCaracter;
+(* Solucion iterativa
 var
   i,j,k,dimension,espacios: integer;
 BEGIN
@@ -44,9 +86,10 @@ BEGIN
         espacios := espacios - 1;
 
       end;
-
-
-
-
-
 END.
+*)
+
+begin
+  TrianguloDeCaracterRecursivo('x', 5);
+end.
+
